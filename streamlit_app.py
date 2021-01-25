f024da2570d029b2ec1d129c484e5026560807c4
guyscottsouth <41329919+guyscottsouth@users.noreply.github.com>
2021-01-25 11:56:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,8 @@ import streamlit as st
 import numpy as np
 
 """
-# Welcome to Guy's Streamlit!
+# Trading Report
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 def get_dataframe():
